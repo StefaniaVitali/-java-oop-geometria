b2c7979b7630331de7ba0cf5762eefbab5786c96
StefaniaVitali <stefy.egypt@hotmail.it>
2024-04-23 11:24:31 +0200

    ADD: class Rettangolo with constructor and methods

--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,5 +1,38 @@
+/* Consegna: Nel progetto java-oop-geometria creare un package org.lessons.java.geometria in cui inserire le classi
+1. Creare una classe Rettangolo con due attributi interi: base e altezza. 
+Aggiungere un opportuno costruttore con parametri. 
+Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+2. Creare una classe Main contenente il metodo main(), 
+nel quale chiedete all’utente di inserire, da console, i valori di base e di altezza con cui istanziare 
+un nuovo Rettangolo. Dopo averlo istanziato, stampate a video il perimetro e l’area.
+BONUS Aggiungere alla classe Rettangolo un metodo disegna che disegna 
+in console il rettangolo con le sue dimensioni, come nell’immagine allegata
+ * 
+ * */
+
 package org.lessons.java.geometria;
 
+
 public class Rettangolo {
+	
+	 public int base;
+	 public int altezza;
+	 
+	 public Rettangolo(int base, int altezza){
+		 
+		 this.base = base;
+		 this.altezza = altezza;		 
+		 
+	 }
+	 
+	 int calcolaPerimetro (int a, int b) {
+		 int perimetro = 2*a + 2*b;
+		 return perimetro;
+	 }
+	 
+	 int calcolaArea (int a, int b) {
+		 int area = a * b;
+		 return area;
+	 }
 
 }
